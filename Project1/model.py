92a1c7d1c85b1bb4fe17dcae8c95b74e2f2c7ff5
Wu-XinYuan <1145413987@qq.com>
2021-10-17 15:00:39 +0800

    modified additive smoothing
    with min ppl around 9000

--- Project1/model.py
+++ Project1/model.py
@@ -7,51 +7,43 @@ import os
 from smooth import smooth, find_best_smooth, perplexity
 
 
-def load_data(data_dir):
+def load_data(data_dir, file_name, nn):
     """
+    加载数据并预处理，将出现频率少于n=3次的词替换为<UNK>
     Args:
         data_dir: relative or absolute path of dataset folder,
-        which include three .txt file: 'train_set.txt', 'dev_set.txt', 'test_set.txt'
+        file_name: 'train_set.txt' 或 'dev_set.txt' 或 'test_set.txt'
+        nn: ngram中的n
     Returns:
-        three part of data in the form of list
+        预处理后的数据
     """
-    print('loading data ...')
-    train_path = os.path.join(data_dir, 'train_set.txt')
-    with open(train_path) as f:
-        train_data = [l.strip() for l in f.readlines()]
-    train_data = ''.join(train_data).split(' ')
-    print('loading train data from {} with total {} words'.format(train_path, len(train_data)))
-
-    dev_path = os.path.join(data_dir, 'dev_set.txt')
-    with open(dev_path) as f:
-        dev_data = [l.strip() for l in f.readlines()]
-    dev_data = ''.join(dev_data).split(' ')
-    print('loading dev data from {} with total {} words'.format(dev_path, len(dev_data)))
-
-    test_path = os.path.join(data_dir, 'test_set.txt')
-    with open(test_path, 'r') as f:
-        test_data = [l.strip() for l in f.readlines()]
-    test_data = ''.join(test_data).split(' ')
-    print('loading train data from {} with total {} words'.format(test_path, len(test_data)))
-    print('loading done!!!')
-
-    return train_data, dev_data, test_data
+    file_path = os.path.join(data_dir, file_name)
+    with open(file_path) as f:
+        data = [l.strip() for l in f.readlines()]
+    data = ''.join(data).split(' ')
+    print('loading data from {} with total {} words'.format(file_path, len(data)))
+    for _ in range(nn-1):  # 手动padding
+        data.insert(0, '<s>')
+        data.append('</s>')
+    frequency = nltk.FreqDist(data)
+    return [word if frequency[word] >= 3 else '<UNK>' for word in data]
 
 
 if __name__ == '__main__':
     # Load and prepare data
-    data_path = r'./dataset'
-    trainData, devData, testData = load_data(data_path)
-    print("=" * 50)
+    data_path = r'dataset'
     n = 3
-    n_grams = nltk.ngrams(trainData, n, pad_right=True, pad_left=True, left_pad_symbol='<s>', right_pad_symbol='</s>')
+    trainData = load_data(data_path, 'train_set.txt', n)
+    devData = load_data(data_path, 'dev_set.txt', n)
+    testData = load_data(data_path, 'test_set.txt', n)
+    print("=" * 50)
+    n_grams = nltk.ngrams(trainData, n)
     vocab = nltk.lm.Vocabulary(trainData + ['<s>', '</s>'])
     freq = nltk.FreqDist(n_grams)
     smoothChoice, smoothParam = find_best_smooth(trainData, n_grams, n, freq, devData)
     freq_s = smooth(trainData, n_grams, n, freq, smoothChoice, smoothParam)
     ppl = perplexity(n, testData, freq_s)
     print('ppl:{}', ppl)
-
 
     print('=' * 50)
 #    print(lm.perplexity.py(testData))
--- Project1/smooth.py
+++ Project1/smooth.py
@@ -1,12 +1,42 @@
 import nltk
 import math
+import itertools
+
+use_n1 = 0
 
 
-def perplexity(nn, test_data, model):
-    print('calculating perplexity')
-    test_ngrams = nltk.ngrams(test_data, nn, pad_right=True, pad_left=True, left_pad_symbol='<s>',
-                              right_pad_symbol='</s>')
-    test_ngrams = [ngram if (ngram in model) else '<UNK>' for ngram in test_ngrams]  # 把不在模型中的词组置为'<UNK>'
+def substitute(ngram, model, n):
+    """
+    如果三元组在语言模型中不存在，用<UNK>进行适当代替，如果一个词替换为<UNK>后在语言模型中，则将其中一个词替换，
+    否则，接着尝试测试两个词、三个词
+    Args:
+        ngram: 等待被替换的测试集的n元词组
+        model: 语言模型
+        n: ngram中的n
+
+    Returns:
+        被替换后的ngram
+    """
+    global use_n1
+    ngram_sub = ngram[:-1]+('<UNK>',)
+    if ngram in model:
+        return ngram
+    if ngram_sub in model:
+        use_n1 += 1
+        return ngram_sub
+    else:
+        return 'UNK'  # 从未出现过的词组
+
+
+def perplexity(n, test_data, model):
+    # print('calculating perplexity')
+    test_ngrams = nltk.ngrams(test_data, n)
+    global use_n1
+    use_n1 = 0
+    test_ngrams = [substitute(ngram, model, n) for ngram in test_ngrams]  # 把不在模型中的词组置为'<UNK>'
+    # print('use n-1 grams:', use_n1)
+    # test_ngrams = [ngram if ngram in model else 'UNK' for ngram in test_ngrams]
+    # test_ngrams = list(test_ngrams)
     probabilities = [model[ngram] for ngram in test_ngrams]
     N = len(probabilities)
     return math.exp((-1 / N) * sum(map(math.log, probabilities)))
@@ -17,7 +47,6 @@ def additive_smoothing(data, n, freq, delta):
     加法平滑法，所有概率均加1
     Args:
         n: n_gram中的n
-        ngrams: 所有n元组
         delta: 参数
         data: 语料库
         freq: 列表，词组出现的频率
@@ -33,44 +62,67 @@ def additive_smoothing(data, n, freq, delta):
     def additive(gram, count):
         n1_gram = gram[:-1]
         n1_count = n1_freq[n1_gram]
+        if n1_count == 0:
+            print('frequency of', n1_gram, 'is 0')
         return (count + delta) / (n1_count + delta * V)
+
     model = {gram: additive(gram, count) for gram, count in freq.items()}
-    model['<UNK>'] = 1/V
+    global cnt
+    for gram, freq in n1_freq.items():  # 对于三元组（a,b,c)如果(a,b)出现过，那么概率就是delta/(P(ab)+delta*v)
+        if gram+('<UNK>',) not in model:
+            model[gram+('<UNK>',)] = delta / (freq + delta * V)
+    model['UNK'] = 1/V  # 那些频率为0的项的概率
     return model
 
 
-def GoodTuring_smooth(data, ngrams, n, freq, delta):
+def goodTuring_smooth(data, n, freq):
     """
-    加法平滑法，所有概率均加1
+    Good-Turing平滑法，原来出现r次的语句变成(r+1)*n_(r+1)/n_r次,
+    因为n_r可能为0, 所以这里做出一些变化，改为原来出现r次的语句变成s*n_s/n_r次,s为从r+1往上数第一个满足n_s不为0的数
     Args:
-        n: n_gram中的n
-        ngrams: 所有n元组
-        delta: 参数
-        data: 语料库
         freq: 列表，词组出现的频率
+        data: 语料库
+        n: n_gram中的n
 
     Returns:
         dict, 每个词组与其概率对应
-        p_unk: 没出现过的词组的概率
     """
     V = len(data) + n - 1  # 词组总数, 因为两边都有padding
-    n1_grams = nltk.ngrams(data, n - 1)
-    n1_freq = nltk.FreqDist(n1_grams)
-
-    def additive(gram, count):
-        n1_gram = gram[:-1]
-        n1_count = n1_freq[n1_gram]
-        return (count + delta) / (n1_count + delta * V)
-    model = {gram: additive(gram, count) for gram, count in freq.items()}
-    model['<UNK>'] = 1/V
-    return model
+    freq['<UNK>'] = 0
+    grams = list(freq)
+    fre = list(freq.values())
+    values = list(set(fre))
+    values.sort(reverse=True)  # 所有可能的r从大到小排列
+    s = values[0]+1
+    s_cnt = 0
+    for r in values:
+        r_cnt = fre.count(r)
+        r_new = s * s_cnt / r_cnt
+        fre = [r_new if val == r else val for val in fre]
+        s, s_cnt = r, r_cnt
+    fre = [nr/V for nr in fre]
+    return dict(zip(grams, fre))
 
 
 def find_best_smooth(data, ngram, n, freq, test_data):
+    """
+
+    Args:
+        data:
+        ngram:
+        n:
+        freq:
+        test_data:
+
+    Returns:
+        ppl最小的方案：
+            1 -- additive
+            2 -- Good-Turing
+    """
     min_ppl = float('inf')
     best_choice = 0
     best_param = 0
-    for delta in [1e-6, 1e-5, 1e-4, 1e-3, 0.01, 0.1, 1]:
+    for delta in [1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 0.01, 0.1, 1]:
         print('testing additive smoothing with delta({})...'.format(delta))
         lm = additive_smoothing(data, n, freq, delta)
         ppl = perplexity(n, test_data, lm)
@@ -79,7 +131,17 @@ def find_best_smooth(data, ngram, n, freq, test_data):
             best_choice = 1
             best_param = delta
             min_ppl = ppl
+
+    print('testing Good-Turing smoothing...')
+    # lm = goodTuring_smooth(data, n, freq)
+    # ppl = perplexity(n, test_data, lm)
+    # print('  ppl: ', ppl)
+    # if ppl < min_ppl:
+    #     best_choice = 2
+    #     min_ppl = ppl
+
     print('best choice: {} best parameter:{} best ppl: {}'.format(best_choice, best_param, min_ppl))
+
     return best_choice, best_param
 
 
@@ -100,3 +162,5 @@ def smooth(data, ngram, n, freq, choice, param):
     print('smoothing model ...')
     if choice == 1:
         return additive_smoothing(data, n, freq, param)
+    if choice == 2:
+        return goodTuring_smooth(data, n, freq)
